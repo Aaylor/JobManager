523bb6230d2325a1553b83b0a0e487ccdb751547
aaylor <loic.runarvot@gmail.com>
2014-07-08 16:54:32 +0200

    Add DateInterval

---
+++ src/DateInterval.cs
@@ -0,0 +1,21 @@
+namespace JobManager.Date {
+
+    public sealed class DateInterval {
+
+        public int Years        = 0;
+
+        public int Months       = 0;
+
+        public int Days         = 0;
+
+        public int Hours        = 0;
+
+        public int Minutes      = 0;
+
+        public int Seconds      = 0;
+
+        public int Milliseconds = 0;
+    
+    }
+
+}
